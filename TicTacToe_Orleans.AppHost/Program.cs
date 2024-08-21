cd0f2d7dc8d0a2b064dcc47ef99d15662f4f8ad9
Samson Amaugo <sammychinedu2ky@gmail.com>
2024-08-21 11:35:20 +0100

    aspire in view

--- TicTacToe_Orleans.AppHost/Program.cs
+++ TicTacToe_Orleans.AppHost/Program.cs
@@ -1,7 +1,20 @@
+using Microsoft.AspNetCore.DataProtection;
+using Microsoft.Extensions.Configuration;
+
 var builder = DistributedApplication.CreateBuilder(args);
 
+var db = builder.AddPostgres("postgresdb")
+                .AddDatabase("tictactoe");
 var redis = builder.AddRedis("redis");
+var backend = builder.AddProject<Projects.TicTacToe_Orleans>("tictactoe-orleans")
+    .WithReference(redis)
+    .WithReference(db);
 
-builder.AddProject<Projects.TicTacToe_Orleans>("tictactoe-orleans").WithReference(redis);
+var frontend = builder.AddNpmApp("webclient", "../webapp", "dev")
+    .WithHttpEndpoint(env: "PORT")
+    .WithExternalHttpEndpoints()
+    .WithReference(backend);
+
+backend.WithReference(frontend);
 
 builder.Build().Run();

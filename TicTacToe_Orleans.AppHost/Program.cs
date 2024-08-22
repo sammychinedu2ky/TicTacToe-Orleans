cf2511581795dbe878196485c8eceff047ebee0c
Samson Amaugo <sammychinedu2ky@gmail.com>
2024-08-22 19:07:32 +0100

    aspire to inspire

--- TicTacToe_Orleans.AppHost/Program.cs
+++ TicTacToe_Orleans.AppHost/Program.cs
@@ -8,6 +8,9 @@ var db = builder.AddPostgres("postgresdb")
 var redis = builder.AddRedis("redis");
 
 
+builder.AddProject<Projects.DataBaseMigrator>("databasemigrator")
+    .WithReference(db);
+
 var backend = builder.AddProject<Projects.TicTacToe_Orleans>("tictactoe-orleans")
     .WithReference(redis)
     .WithReference(db)
@@ -16,8 +19,6 @@ var backend = builder.AddProject<Projects.TicTacToe_Orleans>("tictactoe-orleans"
     .WithEndpoint(name: "ORLEANS-GATEWAY-PORT", port: 877, scheme: "http", env: "ORLEANS-GATEWAY-PORT")
     .WithEndpoint(name: "ORLEANS-SILO-DASHBOARD", port: 977, scheme: "http", env: "ORLEANS-SILO-DASHBOARD");
    
-builder.AddProject<Projects.DataBaseMigrator>("databasemigrator")
-    .WithReference(db);
 
 var frontend = builder.AddNpmApp("webclient", "../webapp", "dev")
     .WithHttpEndpoint(env: "PORT", port:3000)
